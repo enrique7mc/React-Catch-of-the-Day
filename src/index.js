6dd5edb5012463229a2657e670f4c8c8aaee83a3
enrique.munguia <enrique.munguia@hulu.com>
2016-10-01 21:08:28 -0500

    set react-router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 import './css/style.css';
 import App from './components/App';
+import NotFound from './components/NotFound';
+import StorePicker from './components/StorePicker';
 
-render(<App />, document.getElementById('main'));
+const Root = () => (
+  <BrowserRouter>
+    <div>
+      <Match exactly pattern="/" component={StorePicker} />
+      <Match exactly pattern="/store/:storeId" component={App} />
+      <Miss component={NotFound} />
+    </div>
+  </BrowserRouter>
+);
+
+render(<Root />, document.getElementById('main'));
