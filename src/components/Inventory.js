4330cb76f4b4d93ed78960646c71ad4f4ad7dbea
enrique.munguia <enrique.munguia@hulu.com>
2016-11-28 11:22:55 -0600

    fix inventory header

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -5,7 +5,7 @@ class Inventory extends React.Component {
   render() {
     return (
       <div>
-        <p>Inventory</p>
+        <h2>Inventory</h2>
         <AddFishForm addFish={this.props.addFish} />
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
